0be4d7019fc9e20c57e39c23e6193356c115e850
TaiHeng W <taihengw@gmail.com>
2015-07-01 15:30:41 +0800

    Add Dennis Ritchie Edition strcat, See it in his C book page 47

--- strcat.c
+++ strcat.c
@@ -18,6 +18,16 @@ char * my_strcat( char *dest, const char *src )
     return dest;
 }
 
+/* This a Dennis M. Ritchie Edition. I see it in  */
+/* <<The C Programming Language>>(Second Edition) */
+/* He didn't use head file <string.h>. */
+char * Dennis_strcat(char *dest, const char *src)
+{
+    int i, j;
+    i = j = 0;
+    while(dest[i++] != '\0') { ++i; }
+    while((dest[i++] = src[j++]) != '\0');
+}
 int main(int argc, char *argv[])
 {
     char str[50] = "Hello ";
@@ -25,6 +35,13 @@ int main(int argc, char *argv[])
     my_strcat(str, str2);
     my_strcat(str, " ...");
     my_strcat(str, " Goodbye World!");
+    puts(str);
+
+    char des[50] = "Hello ";
+    char rit[50] = "World!";
+    Dennis_strcat(des, rit);
+    Dennis_strcat(des, " ...");
+    Dennis_strcat(des, " Goodbye World!");
     puts(str);
     return 0;
 }
